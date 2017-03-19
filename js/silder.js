75dbafab8ebec014f5a31e5da571a88537275f4c
caishijian2008 <442754043@qq.com>
2017-03-19 15:06:52 +0800

    添加划到顶部功能

--- js/silder.js
+++ js/silder.js
@@ -1,13 +1,29 @@
 /*导航当前页高亮*/
 var obj=null;
-var As=document.getElementById('topnav').getElementsByTagName('a');
-obj = As[0];
-for(i=1;i<As.length;i++) {
-	if(window.location.href.indexOf(As[i].href)>=0) {
-        obj=As[i];
+var topnav=document.getElementById('topnav').getElementsByTagName('a');
+obj = topnav[0];
+for(i=1;i<topnav.length;i++) {
+	if(window.location.href.indexOf(topnav[i].href)>=0) {
+        obj=topnav[i];
 	}
 }
 obj.id='topnav_current';
 
 /*百度分享广告*/
 window._bd_share_config={"common":{"bdSnsKey":{},"bdText":"","bdMini":"1","bdMiniList":false,"bdPic":"","bdStyle":"1","bdSize":"32"},"share":{}};with(document)0[(getElementsByTagName('head')[0]||body).appendChild(createElement('script')).src='http://bdimg.share.baidu.com/static/api/js/share.js?v=89860593.js?cdnversion='+~(-new Date()/36e5)];
+
+  /* scroll to top JS */
+  $(".totop-btn").hide();
+  $(function() {
+	  $(window).scroll(function(e) {
+	    if($(window).scrollTop() >= 100) {
+	      $(".totop-btn").fadeIn();
+	    } else {
+	      $(".totop-btn").fadeOut();
+	    }
+	  });
+  });
+  $(".totop-btn").click(function(e) {
+    $("html, body").animate({scrollTop: 0}, 300); 
+    return false; 
+  }); 
